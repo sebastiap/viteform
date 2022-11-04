c423bff13a5c667fcead3b01565e7df73d1e95d8
sebastiap <sebastiap@gmail.com>
2022-11-04 19:07:53 -0300

    useAPI hook with useReducer

--- src/components/API/index.jsx
+++ src/components/API/index.jsx
@@ -1,50 +1,89 @@
-import { useState,useEffect } from "react";
-import axios from "axios";
+import React, {   useState, useEffect } from 'react';
+import axios from 'axios';
+
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    default:
+      throw new Error();
+  }
+};
+
+const useDataApi = (initialUrl, initialData) => {
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData,
+  });
+
+  // const [data, setData] = useState(initialData);
+  const [url, setUrl] = useState(initialUrl);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_INIT' });
+
+      try {
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAILURE' });
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [url]);
+
+  return [state, setUrl];
+};
 
 
 const ApiProp = () => {
-    const [data, setData] = useState({ hits: [] });
     const [query, setQuery] = useState('redux');
-    const [url, setUrl] = useState(
+    const [{ data, isLoading, isError }, doFetch] = useDataApi(
       'https://hn.algolia.com/api/v1/search?query=redux',
+      { hits: [] },
     );
 
-    const [isLoading,setLoading] = useState(false)
-    const [isError, setIsError] = useState(false);
-
-    useEffect(() => {
-      const fetchData = async () => {
-        setIsError(false);
-        setLoading(true);
-        try
-        {        
-          const result = await axios(url);
-          setData(result.data);
-        }
-        catch (error) {
-          setIsError(true);
-        }
-        setLoading(false);
-      };
-  
-      fetchData();
-    }, [url]);
-    
     return (
         <>
-      <input
-        type="text"
-        value={query}
-        onChange={event => setQuery(event.target.value)}
-      />
-      <button
-        type="button"
-        onClick={() =>
-          setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)
-        }
+      <form
+        onSubmit={event => {
+          doFetch(
+            `http://hn.algolia.com/api/v1/search?query=${query}`,
+          );
+
+          event.preventDefault();
+        }}
       >
-        Search
-      </button>
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
 
       {isError && <div>Something went wrong ...</div>}
 
@@ -52,11 +91,11 @@ const ApiProp = () => {
         <div>Loading ...</div>
       ) : (
         <ul>
-        {data.hits.map( item => (
-            <li key={item.objectId}>
-                <a href={item.url}>{item.title}</a>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
             </li>
-            ))}
+          ))}
         </ul>
       )}
         </>
---
+++ src/components/common/useApi.jsx
@@ -0,0 +1,31 @@
+import { useState,useEffect} from 'react';
+
+const useHackerNewsApi = () => {
+    const [data, setData] = useState({ hits: [] });
+    const [url, setUrl] = useState(
+      'https://hn.algolia.com/api/v1/search?query=redux',
+    );
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        setIsError(false);
+        setIsLoading(true);
+  
+        try {
+          const result = await axios(url);
+  
+          setData(result.data);
+        } catch (error) {
+          setIsError(true);
+        }
+  
+        setIsLoading(false);
+      };
+  
+      fetchData();
+    }, [url]);
+  
+    return [{ data, isLoading, isError }, setUrl];
+  }
